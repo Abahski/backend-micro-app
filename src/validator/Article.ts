c4e342f374b0b1aef2d93e217b6946e2e183a539
Abahski <liswarareja@gmail.com>
2024-03-13 10:40:59 +0700

    Fix for validator

--- src/validator/Article.ts
+++ src/validator/Article.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 
 export const ArticleValidator = Joi.object({
-	fullname: Joi.string().required(),
+	title: Joi.string().required(),
 	image: Joi.string().required(),
 	content: Joi.string().required(),
 	description: Joi.string().required(),
